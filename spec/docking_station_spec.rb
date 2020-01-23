9b54f63bd5eb8eb5e02b656b3db6ccc68b9d833e
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-01-23 14:52:51 +0000

    Complete challenge 14

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,27 +1,20 @@
 require 'docking_station'
 
+
 describe DockingStation do
     it { is_expected.to respond_to(:release_bike) }
 
     it { is_expected.to respond_to(:dock).with(1).argument }
 
-    it { is_expected.to respond_to(:bike) }
-
     it 'docks something' do
         bike = Bike.new
-        expect(subject.dock(bike)).to eq bike
-    end
-
-    it 'returns docked bikes' do
-        bike = Bike.new
-        subject.dock(bike)
-        expect(subject.bike).to eq bike
+        expect(subject.dock(bike)).to eq [bike]
     end
 
     describe '#dock' do
         it 'raises an error when full' do
-            subject.dock(Bike.new)
-            expect { subject.dock Bike.new }.to raise_error('No space available')
+            20.times { subject.dock(Bike.new) }
+            expect { subject.dock Bike.new }.to raise_error('Docking station full')
         end
     end
 
