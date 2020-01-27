6fb2dd5c7afaf3d68bc3135099bacb6e90b6947c
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-01-27 13:05:35 +0000

    Add report_broken method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,12 @@
 require 'docking_station'
 
 describe Bike do
-    it { is_expected.to respond_to(:working?) }
+    it { is_expected.to respond_to(:broken?) }
+
+    it 'can be reported broken' do
+        subject.report_broken
+        expect(subject).to be_broken
+    end
 end
 
 
